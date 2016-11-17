81f856b36b08e57500826dc648eab520b81dd7ca
Ricardo Münch <munch_141@hotmail.com>
2016-11-16 22:35:24 -0400

    arreglé el foldr de Array e implementé el foldr del árbol (Rose).

--- ruby/foldable.rb
+++ ruby/foldable.rb
@@ -1,12 +1,38 @@
 #!/usr/bin/ruby
 
 class Array
-    def foldr e, b
+    def foldr e
         if self == [] then
             e
         else
             head, *tail = *self
-            b.call(head, tail.foldr(e,b))
+            yield(head, tail.foldr(e, &Proc.new))
+        end
+    end
+end
+
+class Rose
+    attr_accessor :elem, :children
+    def initialize elem, children = []
+        @elem = elem
+        @children = children
+    end
+
+    def add elem
+        @children.push elem
+        self
+    end
+
+    def foldr e
+        if children == [] then
+            yield(@elem, e)
+        else
+            yield(
+                @elem,
+                children.foldr(e) do
+                    |child, acc| yield(child.foldr(e, &Proc.new), acc)
+                end
+            )
         end
     end
 end
