eb058b4adbef5757a3cda5a4362e2a84c1af75ea
ricardo <munch_141@hotmail.com>
2016-11-21 14:51:27 -0400

    arreglé el foldr del árbol y cambié la firma de los foldr para que reciba el bloque explícitamente

--- ruby/foldable.rb
+++ ruby/foldable.rb
@@ -1,18 +1,40 @@
 #!/usr/bin/ruby
 
+module Foldable
+    def null?
+       self.foldr(true) {|_,_| false}
+    end
+
+    def foldr1 &b
+        if self.null? then
+            raise "empty structure"
+        else
+            # falta esto
+        end
+    end
+
+    def length
+        self.foldr(0) {|_,l| l+1}
+    end
+end
+
 class Array
-    def foldr e
+    include Foldable
+
+    def foldr e, &b
         if self == [] then
             e
         else
-            head, *tail = *self
-            yield(head, tail.foldr(e, &Proc.new))
+            head, *tail = self
+            yield(head, tail.foldr(e, &b))
         end
     end
 end
 
 class Rose
     attr_accessor :elem, :children
+    include Foldable
+
     def initialize elem, children = []
         @elem = elem
         @children = children
@@ -23,16 +45,9 @@ class Rose
         self
     end
 
-    def foldr e
-        if children == [] then
-            yield(@elem, e)
-        else
-            yield(
-                @elem,
-                children.foldr(e) do
-                    |child, acc| yield(child.foldr(e, &Proc.new), acc)
-                end
-            )
-        end
+    def foldr e, &b
+        r = e
+        @children.reverse_each {|child| r = child.foldr(r, &b)}
+        b.call(@elem, r)
     end
 end
